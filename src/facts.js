62a7e8172a83e6093fd7d54e1aaf2f77a7cb6072
Dom Sykes <dominicjsykes@gmail.com>
2017-07-25 21:06:34 +0100

    Updated Facts and Utterances

--- src/facts.js
+++ src/facts.js
@@ -2,11 +2,15 @@
 // Include at least 10 distinct facts with a 4-digit year included in the text for each
 module.exports = {
     "FACTS_EN": [
-        "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
-        "The term, Artificial Intelligence, was coined in 1956 by mathematician and computer scientist John McCarthy, at Dartmouth College, in New Hampshire.",
-        "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
-        "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
-        "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "Cholera first spread by trade routes (land and sea) to Russia in 1817, later to the rest of Europe, and from Europe to North America and the rest of the world.",
+        "The causative agent of leprosy, M. leprae, was discovered by G. H. Armauer Hansen in Norway in 1873, making it the first bacterium to be identified as causing disease in humans.",
+        "Sir William Osler, known as 'the father of modern medicine', appreciated the death and disability caused by pneumonia, describing it as the 'captain of the men of death' in 1918, as it had overtaken tuberculosis as one of the leading causes of death in this time.",
+        "The oral polio vaccine was developed by Albert Sabin and came into commercial use in 1961.",
+        "Thomas Hodgkin published the first description of lymphoma in 1832, specifically of the form named after him. Since then, many other forms of lymphoma have been described.",
+        "The last naturally occurring case of smallpox (Variola minor) was diagnosed on 26 October 1977.",
+        "After a global eradication campaign, the last confirmed case of rinderpest was diagnosed in 2001.",
+        "The first written records of an outbreak of syphilis in Europe occurred around 1494 in Naples, Italy, during a French invasion.",
+        "Almost all human cases of rabies were fatal until a vaccine was developed in 1885 by Louis Pasteur and Émile Roux. Their original vaccine was harvested from infected rabbits, from which the virus in the nerve tissue was weakened by allowing it to dry for five to ten days.",
+        "In 1529, a measles outbreak in Cuba killed two-thirds of those natives who had previously survived smallpox. Two years later, measles was responsible for the deaths of half the population of Honduras, and it had ravaged Mexico, Central America, and the Inca civilization."
     ]
 }
